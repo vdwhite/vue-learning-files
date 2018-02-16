306356ac29dcbd9a07506b12557be91278b4bb5b
Guangyun Hou <vdwhite69@gmail.com>
2018-02-16 09:20:57 +0000

    files on C9, without vue-router

---
+++ src/components/Test.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="test">
+        <h1>{{title}}</h1>
+        <input type="text" v-model="title"/><br/>
+        <p v-if="showMe">{{user.firstName}}</p>
+        <p v-else>Nobody</p>        
+        
+        <ul>
+            <li v-for="item in items">{{item.title}}</li>
+        </ul>
+        
+        <button v-on:click="great(title)">Stay greeting</button>
+        <br/>
+        <input type="text" v-on:keyup="keyPress" v-on:keyup.enter="hitEnter"/>
+        
+        <hr/>
+        <label >First name</label><input type="text" v-model="user.firstName">
+        <br/>
+        <label >Last name</label><input type="text" v-model="user.lastName">
+        <br/>
+        <h3>{{fullname}}</h3>
+        <h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'test',
+        props: {
+            msg: {
+                default: "Foo",
+                type:String
+            }
+        },
+        data(){
+            return {
+                title:"Hello World",
+                user:{
+                    firstName: "John",
+                    lastName: "Doe"
+                },
+                showMe:false,
+                items:[
+                    {title:"item1"},
+                    {title:"item2"},
+                    {title:"item3"}
+                ]
+            }
+        },
+        methods:{
+            great:function(msg){
+                alert(msg);
+            },
+            keyPress:function(e){
+                console.log(e.target.value);
+            },
+            hitEnter:function(){
+                alert("You hit enter");
+            }
+            
+        },
+        computed:{
+            fullname: function(){
+                return this.user.firstName + " "+this.user.lastName;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="users">
+        <h1>Users</h1>
+        
+        <form v-on:submit="addUser">
+            <input type="text" v-model="newUser.firstName" placeholder="first name"/>
+            <input type="text" v-model="newUser.lasttName" placeholder="last name"/>
+            <input type="text" v-model="newUser.email" placeholder="email"/>
+            <input type="submit" value="submit"/>
+        </form>
+        
+
+        <ul>
+            <li v-for="user in users">
+                <!--Add the class so if it's contacted, it;s gonna toggle the class-->
+
+                <!--use span: to make the framework possible-->
+                <span :class="{contacted:user.contacted}">
+                    <button v-on:click="deleteUser(user)">Delete User</button>
+                    {{user.firstName}} {{user.lastName}} {{user.email}}
+                    <input type="checkbox" class="toggle" v-model="user.contacted"/>
+                </span>
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'users',
+        data(){
+            return {
+                newUser:{},
+                users:[
+                    {
+                        firstName: "John",
+                        lastName: "Doe",
+                        email:"example@mail.com",
+                        contacted:false
+                    },
+                    {
+                        firstName: "Ben",
+                        lastName: "Frank",
+                        email:"bexample@mail.com",
+                        contacted:false
+                    },
+                    {
+                        firstName: "Casde",
+                        lastName: "Fack",
+                        email:"csle@mail.com",
+                        contacted:false
+                    }
+                ]
+            }
+        },
+        methods:{
+            addUser:function(e){
+                //stop doing the refershing
+                e.preventDefault();
+                this.users.push(
+                    {
+                        firstName: this.newUser.firstName,
+                        lastName: this.newUser.lastName,
+                        email: this.newUser.email,
+                        contacted: false
+                    });
+            },
+            
+            deleteUser: function(user){
+                this.users.splice(this.users.indexOf(user),1);
+            }
+            
+            
+        },
+        computed:{
+
+        }
+    }
+</script>
+
+<style scoped>
+    .contacted{
+        text-decoration: line-through;
+    }
+</style>
